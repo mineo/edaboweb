f670300d49a435e59768a555a13d5795bdbfc36e
Wieland Hoffmann <themineo@gmail.com>
2015-03-14 23:05:24 +0100

    create tables on startup

--- run.py
+++ run.py
@@ -4,4 +4,8 @@
 # License: MIT, see LICENSE for details
 if __name__ == "__main__":
     from edaboweb.edaboweb import app
+    from edaboweb.models import db
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
     app.run(debug=True)
