cbc326da60c5b766db803cbbf394f316e7c1aa73
Wieland Hoffmann <themineo@gmail.com>
2015-03-14 23:16:58 +0100

    Add flask-uuid

--- edaboweb/blueprints/playlist.py
+++ edaboweb/blueprints/playlist.py
@@ -2,7 +2,8 @@
 # coding: utf-8
 # Copyright © 2015 Wieland Hoffmann
 # License: MIT, see LICENSE for details
-from flask import abort, Blueprint, render_template
+from flask import Blueprint, render_template
+from ..models import Playlist
 
 playlist_bp = Blueprint("playlist", __name__)
 
@@ -10,3 +11,9 @@ playlist_bp = Blueprint("playlist", __name__)
 @playlist_bp.route("/")
 def list_playlists():
     return render_template("playlist/list.html")
+
+
+@playlist_bp.route("/<uuid:pid>", methods=["GET"])
+def view_playlist(pid):
+    playlist = Playlist.query.filter(Playlist.gid == str(pid)).first_or_404()
+    return render_template("playlist/single.html", playlist=playlist)
--- edaboweb/edaboweb.py
+++ edaboweb/edaboweb.py
@@ -3,12 +3,14 @@
 # Copyright © 2015 Wieland Hoffmann
 # License: MIT, see LICENSE for details
 from flask import Flask
+from flask.ext.uuid import FlaskUUID
 
 from .blueprints.index import index_bp
 from .blueprints.playlist import playlist_bp
 from .models import db
 
 app = Flask(__name__)
+FlaskUUID(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://edaboweb@localhost/edaboweb"
 app.register_blueprint(index_bp)
 app.register_blueprint(playlist_bp, url_prefix="/playlist")
