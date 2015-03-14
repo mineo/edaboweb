304bfbcbde1409a73159aec30e6292721403417c
Wieland Hoffmann <themineo@gmail.com>
2015-03-15 00:52:03 +0100

    Add an endpoint to upload data

--- edaboweb/blueprints/playlist.py
+++ edaboweb/blueprints/playlist.py
@@ -2,8 +2,10 @@
 # coding: utf-8
 # Copyright © 2015 Wieland Hoffmann
 # License: MIT, see LICENSE for details
-from flask import Blueprint, render_template
-from ..models import Playlist
+from flask import abort, Blueprint, redirect, request, render_template, url_for
+from json import loads
+from uuid import UUID
+from ..models import db, Playlist
 
 playlist_bp = Blueprint("playlist", __name__)
 
@@ -17,3 +19,22 @@ def list_playlists():
 def view_playlist(pid):
     playlist = Playlist.query.filter(Playlist.gid == str(pid)).first_or_404()
     return render_template("playlist/single.html", playlist=playlist)
+
+
+@playlist_bp.route("/<uuid:pid>", methods=["POST"])
+def add_playlist(pid):
+    doc = request.get_data()
+    json = loads(doc.encode("utf-8"))
+
+    uuid_from_doc = UUID(json["uuid"])
+    if uuid_from_doc != pid:
+        abort(400)
+
+    playlist = Playlist.query.filter(Playlist.gid == str(pid)).first()
+    if playlist is None:
+        playlist = Playlist(gid=str(pid), data=json)
+    else:
+        playlist.data = json
+    db.session.add(playlist)
+    db.session.commit()
+    return redirect(url_for('playlist.list_playlists'))
