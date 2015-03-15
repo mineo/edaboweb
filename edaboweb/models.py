26fc1ba0358be6b3123e1ca3912814361731a741
Wieland Hoffmann <themineo@gmail.com>
2015-03-15 01:33:08 +0100

    some more work

--- edaboweb/models.py
+++ edaboweb/models.py
@@ -3,11 +3,12 @@
 # Copyright © 2015 Wieland Hoffmann
 # License: MIT, see LICENSE for details
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 
 db = SQLAlchemy()
 
 
 class Playlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(JSON)
+    gid = db.Column(UUID, nullable=False)
+    data = db.Column(JSONB, nullable=False)
