4a5b0c35adca45b592246006fb7badb15b6ef547
Wieland Hoffmann <themineo@gmail.com>
2016-10-30 23:44:35 +0100

    Remove test data

--- edaboweb/ws_models.py
+++ edaboweb/ws_models.py
@@ -15,16 +15,3 @@ Playlist = Schema({"name": Use(unicode),
                    "tracklist": [Track],
                    "uuid": Use(UUID)
                    })
-
-pl = {
-  "tracklist": [
-    {
-      "releasetrackid": "1eb2363b-83b1-3527-a845-1164eb3f04ed",
-      "recordingid": "4e0b973e-219b-47fb-b3a1-074695e837d6"
-    }
-  ],
-  "name": "test2",
-  "timestamp": "2015-01-25T22:01:28.202Z",
-  "description": None,
-  "uuid": "94b02773-a640-464c-b1cd-7e73908e3728"
-}
